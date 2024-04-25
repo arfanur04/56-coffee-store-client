c04a75a9883edccb3a61d7c35dab3fc0912c3516
arfanur04 <arfanur04@gmail.com>
2024-04-25 12:04:41 +0600

    56-3 Server Side Hide Database User And Password Using Dotenv Config

--- src/layouts/Roots/Roots.jsx
+++ src/layouts/Roots/Roots.jsx
@@ -3,11 +3,13 @@ import Navbar from "../../pages/Shared/Navbar/Navbar";
 
 const Roots = () => {
 	return (
-		<div>
-			<Navbar />
-			<h1 className="text-5xl text-purple-600 text-center">Coffee Store</h1>
-			<Outlet />
-		</div>
+		<>
+			<div className="max-w-6xl mx-auto px-2">
+				<Navbar />
+				<h1 className="text-5xl text-purple-600 text-center">Coffee Store</h1>
+				<Outlet />
+			</div>
+		</>
 	);
 };
 
--- src/pages/AddCoffee/AddCoffee.jsx
+++ src/pages/AddCoffee/AddCoffee.jsx
@@ -8,7 +8,37 @@ const AddCoffee = () => {
 				<title>{websiteTitle} - AddCoffee</title>
 			</Helmet>
 			<div>
-				<h2>This is AddCoffee</h2>
+				<h2 className="ml-24">Add a Coffee</h2>
+				<form>
+					<div className="flex gap-6">
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Coffee Name</span>
+							</label>
+							<label>
+								<input
+									type="text"
+									placeholder="Coffee Name"
+									className="input input-bordered"
+									required
+								/>
+							</label>
+						</div>
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Available Quantity</span>
+							</label>
+							<label>
+								<input
+									type="text"
+									placeholder="Available Quantity"
+									className="input input-bordered"
+									required
+								/>
+							</label>
+						</div>
+					</div>
+				</form>
 			</div>
 		</>
 	);
